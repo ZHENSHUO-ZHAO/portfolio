e5e91fcde651910b1dd498ee735bbd34a405b0ec
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2025-12-10 12:55:07 +1100

    Added ReactRouter library and all the routes needed for this app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { NavLink, Outlet } from "react-router";
+
 function App() {
   return (
     <>
       <div>My Portfolio</div>
+      <nav>
+        <NavLink to="/" end>
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/experience">Experience</NavLink>
+        <NavLink to="/project">Project</NavLink>
+        <NavLink to="/skill">Skill</NavLink>
+        <NavLink to="/education">Education</NavLink>
+        <NavLink to="/hobby">Hobby</NavLink>
+      </nav>
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Experience from "./pages/Experience/Experience";
+import Project from "./pages/Project/Project";
+import Skill from "./pages/Skill/Skill";
+import Education from "./pages/Education/Education";
+import Hobby from "./pages/Hobby/Hobby";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Home />}></Route>
+          <Route path="about" element={<About />}></Route>
+          <Route path="experience" element={<Experience />}></Route>
+          <Route path="project" element={<Project />}></Route>
+          <Route path="skill" element={<Skill />}></Route>
+          <Route path="education" element={<Education />}></Route>
+          <Route path="hobby" element={<Hobby />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
