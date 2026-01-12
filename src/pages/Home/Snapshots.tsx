47fe1eac067533be0ec5cb5504891db2f94793ed
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2026-01-12 13:33:17 +1100

    Added animations to the snapshot cards.

--- src/pages/Home/Snapshots.tsx
+++ src/pages/Home/Snapshots.tsx
@@ -1,3 +1,4 @@
+import { motion, type Variants } from "motion/react";
 import GlowOutline from "../../components/glowFx/GlowOutline";
 import type { DescriptiveItem } from "../../contexts/pageContext";
 import {
@@ -7,33 +8,95 @@ import {
   mixColor,
 } from "../../utils/util";
 
-const outlineGradient: string = createLinearGradient(135, [
-  mixColor(30, "var(--color-accent)", "transparent"),
-  "transparent",
-  "transparent",
-  "transparent",
-  mixColor(30, "var(--color-complement)", "transparent"),
-]);
+const outlineVariants: Variants = {
+  rest: {
+    background: createLinearGradient(135, [
+      mixColor(30, "var(--color-accent)", "transparent"),
+      "transparent",
+      "transparent",
+      "transparent",
+      mixColor(30, "var(--color-complement)", "transparent"),
+    ]),
+  },
+  hover: {
+    background: createLinearGradient(135, [
+      mixColor(60, "var(--color-accent)", "transparent"),
+      "transparent",
+      "transparent",
+      "transparent",
+      mixColor(60, "var(--color-complement)", "transparent"),
+    ]),
+  },
+};
 
-const topLeftGradient: string = createRadialGradientWithStops(
-  undefined,
-  undefined,
-  [
-    [mixColor(30, "var(--color-accent)", "transparent"), 0],
-    ["transparent", 30],
-  ],
-  "top left"
-);
+const topLeftVariants: Variants = {
+  rest: {
+    background: createRadialGradientWithStops(
+      undefined,
+      undefined,
+      [
+        [mixColor(30, "var(--color-accent)", "transparent"), 0],
+        ["transparent", 30],
+      ],
+      "top left"
+    ),
+  },
+  hover: {
+    background: createRadialGradientWithStops(
+      undefined,
+      undefined,
+      [
+        [mixColor(50, "var(--color-accent)", "transparent"), 0],
+        ["transparent", 30],
+      ],
+      "top left"
+    ),
+  },
+};
 
-const bottomRightGradient: string = createRadialGradientWithStops(
-  undefined,
-  undefined,
-  [
-    [mixColor(30, "var(--color-complement)", "transparent"), 0],
-    ["transparent", 30],
-  ],
-  "bottom right"
-);
+const bottomRightVariants: Variants = {
+  rest: {
+    background: createRadialGradientWithStops(
+      undefined,
+      undefined,
+      [
+        [mixColor(30, "var(--color-complement)", "transparent"), 0],
+        ["transparent", 30],
+      ],
+      "bottom right"
+    ),
+  },
+  hover: {
+    background: createRadialGradientWithStops(
+      undefined,
+      undefined,
+      [
+        [mixColor(50, "var(--color-complement)", "transparent"), 0],
+        ["transparent", 30],
+      ],
+      "bottom right"
+    ),
+  },
+};
+
+const iconVariants: Variants = {
+  rest: {
+    boxShadow: `${createInnerShadow(
+      "-6px",
+      "-6px",
+      "8px",
+      "rgba(255,255,255,0.8)"
+    )}, ${createInnerShadow("6px", "6px", "10px", "rgba(0,0,0,0.25)")}`,
+  },
+  hover: {
+    boxShadow: `${createInnerShadow(
+      "-6px",
+      "-6px",
+      "8px",
+      "rgba(0,0,0,0.25)"
+    )}, ${createInnerShadow("6px", "6px", "10px", "rgba(255,255,255,0.8)")}`,
+  },
+};
 
 export default function Snapshots({
   snapshot,
@@ -41,25 +104,30 @@ export default function Snapshots({
   snapshot: DescriptiveItem[];
 }) {
   return (
-    <ul className="grid grid-cols-1 min-[489px]:grid-cols-2 lg:grid-cols-4 gap-4">
+    <ul className="grid grid-cols-1 min-[430px]:grid-cols-2 lg:grid-cols-4 gap-4">
       {snapshot.map((s, i) => (
-        <li
+        <motion.li
           key={s.desc}
           className="
-          relative flex flex-col justify-start items-center gap-4 py-4 px-4 rounded-xl border border-slate-100"
-          style={{
-            background: topLeftGradient,
-          }}
+          relative flex flex-col justify-start items-center gap-4 py-4 px-4 rounded-xl bg-slate-500/10"
+          initial="rest"
+          whileHover="hover"
+          whileTap="hover"
         >
-          <div
+          <motion.div
             className="absolute inset-0 rounded-xl"
-            style={{ background: bottomRightGradient }}
+            variants={topLeftVariants}
           />
-          <GlowOutline gradient={outlineGradient} rounded="rounded-xl" />
-          <div className="h-20 flex justify-center items-center">
-            <div
+          <motion.div
+            className="absolute inset-0 rounded-xl"
+            variants={bottomRightVariants}
+          />
+
+          <div className="relative h-20 flex justify-center items-center">
+            <motion.div
               className="h-3/4 aspect-square bg-slate-200 p-3 rounded-full"
-              style={{ boxShadow: createInnerShadow() }}
+              variants={iconVariants}
+              transition={{ ease: "easeIn", duration: 0.3 }}
             >
               {s.icon && (
                 <s.icon className="size-full " stroke={`url(#gradient_${i})`}>
@@ -72,23 +140,25 @@ export default function Snapshots({
                       y2="100%"
                       gradientUnits="userSpaceOnUse"
                     >
-                      <stop
-                        offset="0%"
-                        stopColor="var(--color-accent)"
-                        // stopOpacity="0.3"
-                      />
+                      <stop offset="0%" stopColor="var(--color-accent)" />
                       <stop offset="100%" stopColor="var(--color-complement)" />
                     </linearGradient>
                   </defs>
                 </s.icon>
               )}
-            </div>
+            </motion.div>
           </div>
-          <div className="flex-1 flex flex-col gap-1 justify-start items-center text-center">
-            <p className="font-bold">{s.title}</p>
+
+          <div className="relative flex-1 flex flex-col gap-1 justify-start items-center text-center">
+            <p className="text-lg font-bold">{s.title}</p>
             <p className="text-md font-medium text-neutral-500">{s.desc}</p>
           </div>
-        </li>
+
+          <GlowOutline
+            gradient={{ variants: outlineVariants }}
+            rounded="rounded-xl"
+          />
+        </motion.li>
       ))}
     </ul>
   );
