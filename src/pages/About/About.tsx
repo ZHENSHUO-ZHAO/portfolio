44f26eb4be25beb403c9384133fc2920e0ae296f
ZHENSHUO ZHAO <vincentjill@hotmail.com>
2026-01-14 16:01:33 +1100

    Fixed typo in the file name.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,10 +1,10 @@
 import { useAboutPageContext } from "../../contexts/aboutContext";
-import PageBase, { ArticleList, Section } from "../PageBase";
+import PageBase, { Section } from "../PageBase";
 import { AboutPageHeader } from "./AboutPageHeader";
 import Aim from "./Aim";
 import Journey from "./Journey";
 import Leisure from "./Leisure";
-import Philosophy from "./Philosopy";
+import Philosophy from "./Philosophy";
 
 export default function About() {
   const content = useAboutPageContext();
@@ -15,42 +15,17 @@ export default function About() {
       headerElement={AboutPageHeader}
       headerClassName="pb-8"
     >
-      {/* <Section id="strength-heading" title="My strongest work combines:">
-        <ul>
-          {content.strength.map((s) => (
-            <li key={s}>{s}</li>
-          ))}
-        </ul>
-      </Section> */}
-
       <Section id="journey-heading" title="My Professional Journey">
         <Journey journey={content.journey} />
       </Section>
-
-      {/* <Section id="philosophy-heading" title="My Engineering Philosophy">
-        <ArticleList listData={content.philosophy} />
-      </Section> */}
 
       <Section id="philosophy-heading" title="My Engineering Philosophy">
         <Philosophy philosophy={content.philosophy} />
       </Section>
 
-      {/* <Section id="aim-heading" title="What I'm Looking For">
-        <p>{content.aim.desc}</p>
-        <ul>
-          {content.aim.roles.map((r) => (
-            <li key={r}>{r}</li>
-          ))}
-        </ul>
-      </Section> */}
-
       <Section id="aim-heading" title="What I'm Looking For">
         <Aim desc={content.aim.desc} roles={content.aim.roles} />
       </Section>
-
-      {/* <Section id="leisure-heading" title="Outside of Work">
-        <p>{content.leisure}</p>
-      </Section> */}
 
       <Section id="leisure-heading" title="Outside of Work">
         <Leisure desc={content.leisure.desc} tags={content.leisure.tags} />
